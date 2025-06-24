805e833facab5f65f2709f2e35236e18d55658f9
vamshimudapally <vamshimudapally75699@gmail.com>
2025-06-24 17:42:12 +0530

    16:meetings setup

---
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Meeting id Page</div>
+  )
+}
+
+export default Page
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod';
 import { agentsRouter } from '@/modules/agents/server/procedures';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { createTRPCRouter } from '../init';
+import { meetingssRouter } from '@/modules/meetings/server/procedures';
+
 export const appRouter = createTRPCRouter({
   agents : agentsRouter,
+  meetings: meetingssRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
